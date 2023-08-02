2cb0f225f2a066f642343b075239a11abd849b08
Anirudh <60171262+Blank333@users.noreply.github.com>
2023-08-02 12:37:10 +0530

    Added front-end Added Create customer Added Jackson to parse JSON

--- src/main/java/sunbasedata/apiIntegration/CustomerController.java
+++ src/main/java/sunbasedata/apiIntegration/CustomerController.java
@@ -1,4 +1,5 @@
 package sunbasedata.apiIntegration;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,13 +15,17 @@ public class CustomerController {
     private CustomerService customerService;
 
     @PostMapping("/authenticate")
-    public ResponseEntity<String> authenticateUser(AuthRequest authRequest) {
-
+    public ResponseEntity<String> authenticateUser(@RequestBody AuthRequest authRequest) {
         return new ResponseEntity<>(customerService.login(authRequest), HttpStatus.OK);
     }
 
+    @PostMapping("/create-customer")
+    public ResponseEntity<String> createCustomer(@RequestBody Customer customerRequest) {
+        return new ResponseEntity<>(customerService.create(customerRequest), HttpStatus.CREATED);
+    }
+
     @GetMapping ("/customers")
-    public ResponseEntity<List<Customer>> getAllCustomers() {
+    public ResponseEntity<List<Customer>> getAllCustomers() throws JsonProcessingException {
         return new ResponseEntity<>(customerService.customers(), HttpStatus.OK);
     }
 
--- src/main/java/sunbasedata/apiIntegration/CustomerService.java
+++ src/main/java/sunbasedata/apiIntegration/CustomerService.java
@@ -1,20 +1,24 @@
 package sunbasedata.apiIntegration;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class CustomerService {
     private String bearerToken;
-    public String login(AuthRequest authRequest){
+
+    public String login(AuthRequest authRequest) {
         String apiUrl = "https://qa2.sunbasedata.com/sunbase/portal/api/assignment_auth.jsp";
 
         MultiValueMap<String, String> bodyValues = new LinkedMultiValueMap<>();
@@ -32,21 +36,54 @@ public class CustomerService {
                 .bodyToMono(String.class)
                 .block();
 
+        assert response != null;
         bearerToken = response.trim().replaceAll("[{}\"]", "").split(":")[1];
         return bearerToken;
     }
 
-    public List<Customer> customers(){
+    public List<Customer> customers() throws JsonProcessingException {
         String apiUrl = "https://qa2.sunbasedata.com/sunbase/portal/api/assignment.jsp?cmd=get_customer_list";
-        WebClient client = WebClient.builder().defaultHeader(HttpHeaders.AUTHORIZATION,"Bearer " + bearerToken).build();
+        WebClient client = WebClient.builder().defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + bearerToken).build();
 
-        List<Customer> response = client.get()
+        String response = Objects.requireNonNull(client.get()
+                        .uri(apiUrl)
+                        .retrieve()
+                        .bodyToMono(String.class)
+                        .block())
+                .trim();
+
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(response, new TypeReference<>() {
+        });
+    }
+
+    public String create(Customer customerRequest) {
+        String apiUrl = "https://qa2.sunbasedata.com/sunbase/portal/api/assignment.jsp?cmd=create";
+        WebClient client = WebClient.builder().defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + bearerToken).build();
+
+
+        MultiValueMap<String, String> formData = getStringStringMultiValueMap(customerRequest);
+
+        String response = client.post()
                 .uri(apiUrl)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(formData), Customer.class)
                 .retrieve()
-                .bodyToFlux(Customer.class)
-                .collectList()
+                .bodyToMono(String.class)
                 .block();
 
         return response;
+    }
+
+    private static MultiValueMap<String, String> getStringStringMultiValueMap(Customer customerRequest) {
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("first_name", customerRequest.getFirst_name());
+        formData.add("last_name", customerRequest.getLast_name());
+        formData.add("address", customerRequest.getAddress());
+        formData.add("city", customerRequest.getCity());
+        formData.add("state", customerRequest.getState());
+        formData.add("email", customerRequest.getEmail());
+        formData.add("phone", customerRequest.getPhone());
+        return formData;
     }
 }
